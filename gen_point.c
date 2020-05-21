8d3753eee53de5aaa135d0a5296ad7ac695911fa
AnaelQUEANT <anael.queant@laposte.net>
2020-05-21 15:27:43 +0200

    Adaptation de la génération de point

--- gen_point.c
+++ gen_point.c
@@ -1,41 +1,51 @@
 #include "gen_point.h"
 
-liste_point listePointPolygone;
-int nbPoint;
+polygone * creerPoly(point * unSommet){
+	polygone * lePoly = (polygone*)malloc(1*sizeof(polygone));
+	lePoly->nbPoint = 0;
+	lePoly->sommetPoly = unSommet;
+	lePoly->listePointPoly=(point**)malloc(lePoly->nbPoint*sizeof(point*));
+	return lePoly;
+}
 
 // Vérifier que le point se trouve dans le polygone
-bool pointIsInPoly(point lePoint, polygone lePolygone){
+bool pointIsInPoly(point* lePoint, polygone* lePolygone){
 	bool isInside = false;
+
 	int minX, minY, maxX, maxY;
-	minX = lePolygone.sommets.liste[0]->x;
-	minY = lePolygone.sommets.liste[0]->y;
+	minX = lePolygone->sommetPoly->x;
+	minY = lePolygone->sommetPoly->y;
 	maxX = minX;
 	maxY = minY;
+
 	//Recuperer le maximum et minimum des coordonnées x/y
-	for(int i = 1; i < lePolygone.sommets.nb_point; i++){
-		if(lePolygone.sommets.liste[i]->x < minX){
-			minX = lePolygone.sommets.liste[i]->x;
-		}else if(lePolygone.sommets.liste[i]->x > maxX){
-			maxX = lePolygone.sommets.liste[i]->x;
+	point* sommetSuivant = lePolygone->sommetPoly->next;
+	while(sommetSuivant->x != lePolygone->sommetPoly->x && sommetSuivant->x != lePolygone->sommetPoly->y){
+		if(sommetSuivant->x < minX){
+			minX = sommetSuivant->x;
+		}else if(sommetSuivant->x > maxX){
+			maxX = sommetSuivant->x;
 		}
 
-		if(lePolygone.sommets.liste[i]->y < minY){
-			minY = lePolygone.sommets.liste[i]->y;
-		}else if(lePolygone.sommets.liste[i]->y > maxY){
-			maxY =lePolygone.sommets.liste[i]->y;
+		if(sommetSuivant->y < minY){
+			minY = sommetSuivant->y;
+		}else if(sommetSuivant->y > maxY){
+			maxY = sommetSuivant->y;
 		}
+		sommetSuivant = sommetSuivant->next;
 	}
+
 	//Vérifier que le point se trouve bien dans le carré dont les sommets sont le minimum et le maximum de chaque axe
-	if(lePoint.x < minX || lePoint.x > maxX || lePoint.y < minY || lePoint.y > maxY){
+	if(lePoint->x < minX || lePoint->x > maxX || lePoint->y < minY || lePoint->y > maxY){
 		return false;
 	}
 
 	//Vérifier que le point se trouve dans le polygone en faisant le tour de celui-ci
-	int j = lePolygone.sommets.nb_point - 1;
-	for(int i = 0; i < lePolygone.sommets.nb_point; j = i++){
-		if((lePolygone.sommets.liste[i]->y > lePoint.y) != (lePolygone.sommets.liste[j]->y > lePoint.y)
+	int j = lePolygone->nbPoint;
+	for(int i = 0; i < lePolygone->nbPoint; j = i++){
+		if((lePolygone->listePointPoly[i]->y > lePoint->y) != (lePolygone->listePointPoly[j]->y > lePoint->y)
 		&&
-		lePoint.x < (((lePolygone.sommets.liste[j]->x - lePolygone.sommets.liste[i]->x)*(lePoint.y - lePolygone.sommets.liste[i]->y)) / ((lePolygone.sommets.liste[j]->y - lePolygone.sommets.liste[i]->y) + lePolygone.sommets.liste[i]->x))){
+		lePoint->x < (((lePolygone->listePointPoly[j]->x - lePolygone->listePointPoly[i]->x)*(lePoint->y - lePolygone->listePointPoly[i]->y)) / ((lePolygone->listePointPoly[j]->y - lePolygone->listePointPoly[i]->y) + lePolygone->listePointPoly[i]->x))){
 			isInside = !isInside;
 		}
 	}
@@ -44,92 +54,88 @@ bool pointIsInPoly(point lePoint, polygone lePolygone){
 }
 
 // Vérifier le point de départ et lancer ensuite la création des points
-liste_point gen_point_polygone(polygone lePolygone, point pointDepart, float distanceVoisin){
+bool gen_point_polygone(polygone* lePolygone, point* pointDepart, float distanceVoisin){
 	if(pointIsInPoly(pointDepart, lePolygone) == false){
-		return listePointPolygone;
+		return false;
 	}
-	listePointPolygone.nb_point = lePolygone.sommets.nb_point;
-	listePointPolygone.liste = (point **)malloc(sizeof(point*)*listePointPolygone.nb_point);
-	for(int i = 0; i < listePointPolygone.nb_point; i++){
-		listePointPolygone.liste[i]->x = lePolygone.sommets.liste[i]->x;
-		listePointPolygone.liste[i]->y = lePolygone.sommets.liste[i]->y;
-		listePointPolygone.liste[i]->etat = lePolygone.sommets.liste[i]->etat;
-	}
-	gen_point(lePolygone, pointDepart, distanceVoisin);
-	return listePointPolygone;
+	lePolygone->listePointPoly = (point **) realloc(lePolygone->listePointPoly, ++(lePolygone->nbPoint)*sizeof(point*));
+	lePolygone->listePointPoly[lePolygone->nbPoint - 1] = pointDepart;
+	gen_point(lePolygone, lePolygone->listePointPoly[lePolygone->nbPoint-1], distanceVoisin);
+	return true;
 }
 
 // Générer l'ensemble des points à partir d'un point de départ
-void gen_point(polygone lePolygone, point pointDepart, float distanceVoisin){
-	point nouveauPoint;
+void gen_point(polygone* lePolygone, point* pointDepart, float distanceVoisin){
+	point* nouveauPoint = init_point();
 	//Point à gauche
-	nouveauPoint.x = pointDepart.x - distanceVoisin;
-	nouveauPoint.y = pointDepart.y;
+	nouveauPoint->x = pointDepart->x - distanceVoisin;
+	nouveauPoint->y = pointDepart->y;
 	if(pointIsInPoly(nouveauPoint, lePolygone) == true){
 		bool existant = false;
-		for(int i = 0; i < listePointPolygone.nb_point && existant == false; i++){
-			if(nouveauPoint.x == listePointPolygone.liste[i]->x && nouveauPoint.y == listePointPolygone.liste[i]->y){
+		for(int i = 0; i < lePolygone->nbPoint && existant == false; i++){
+			if(nouveauPoint->x == lePolygone->listePointPoly[i]->x && nouveauPoint->y == lePolygone->listePointPoly[i]->y){
 				existant = true;
 			}
 		}
 		if(!existant){
-			listePointPolygone.liste = (point **)realloc(listePointPolygone.liste, ++(listePointPolygone.nb_point)*sizeof(point*));
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->x = nouveauPoint.x;
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->y = nouveauPoint.y;
-			gen_point(lePolygone, nouveauPoint, distanceVoisin);
+			lePolygone->listePointPoly = (point **)realloc(lePolygone->listePointPoly, ++(lePolygone->nbPoint)*sizeof(point*));
+			lePolygone->listePointPoly[lePolygone->nbPoint - 1] = nouveauPoint;
+			gen_point(lePolygone, lePolygone->listePointPoly[lePolygone->nbPoint-1], distanceVoisin);
+			nouveauPoint = init_point();
 		}
 	}
+
 	//Point à droite
-	nouveauPoint.x = pointDepart.x + distanceVoisin;
-        nouveauPoint.y = pointDepart.y;
+	nouveauPoint->x = pointDepart->x + distanceVoisin;
+	nouveauPoint->y = pointDepart->y;
 	if(pointIsInPoly(nouveauPoint, lePolygone) == true){
 		bool existant = false;
-		for(int i = 0; i < listePointPolygone.nb_point && existant == false; i++){
-			if(nouveauPoint.x == listePointPolygone.liste[i]->x && nouveauPoint.y == listePointPolygone.liste[i]->y){
+		for(int i = 0; i < lePolygone->nbPoint && existant == false; i++){
+			if(nouveauPoint->x == lePolygone->listePointPoly[i]->x && nouveauPoint->y == lePolygone->listePointPoly[i]->y){
 				existant = true;
 			}
 		}
 		if(!existant){
-			listePointPolygone.liste = (point **)realloc(listePointPolygone.liste, ++(listePointPolygone.nb_point)*sizeof(point));
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->x = nouveauPoint.x;
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->y = nouveauPoint.y;
-			gen_point(lePolygone, nouveauPoint, distanceVoisin);
+			lePolygone->listePointPoly = (point **)realloc(lePolygone->listePointPoly, ++(lePolygone->nbPoint)*sizeof(point*));
+			lePolygone->listePointPoly[lePolygone->nbPoint - 1] = nouveauPoint;
+			gen_point(lePolygone, lePolygone->listePointPoly[lePolygone->nbPoint-1], distanceVoisin);
+			nouveauPoint = init_point();
 		}
 	}
+
 
 	//Point en haut
-	nouveauPoint.x = pointDepart.x;
-        nouveauPoint.y = pointDepart.y - distanceVoisin;
-        if(pointIsInPoly(nouveauPoint, lePolygone) == true){
+	nouveauPoint->x = pointDepart->x;
+	nouveauPoint->y = pointDepart->y + distanceVoisin;
+	if(pointIsInPoly(nouveauPoint, lePolygone) == true){
 		bool existant = false;
-		for(int i = 0; i < listePointPolygone.nb_point && existant == false; i++){
-			if(nouveauPoint.x == listePointPolygone.liste[i]->x && nouveauPoint.y == listePointPolygone.liste[i]->y){
+		for(int i = 0; i < lePolygone->nbPoint && existant == false; i++){
+			if(nouveauPoint->x == lePolygone->listePointPoly[i]->x && nouveauPoint->y == lePolygone->listePointPoly[i]->y){
 				existant = true;
 			}
 		}
 		if(!existant){
-			listePointPolygone.liste = (point **)realloc(listePointPolygone.liste, ++(listePointPolygone.nb_point)*sizeof(point));
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->x = nouveauPoint.x;
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->y = nouveauPoint.y;
-			gen_point(lePolygone, nouveauPoint, distanceVoisin);
-		}
-	}
-	//Point en bas
-	nouveauPoint.x = pointDepart.x;
-        nouveauPoint.y = pointDepart.y + distanceVoisin;
-        if(pointIsInPoly(nouveauPoint, lePolygone) == true){
-		bool existant = false;
-		for(int i = 0; i < listePointPolygone.nb_point && existant == false; i++){
-			if(nouveauPoint.x == listePointPolygone.liste[i]->x && nouveauPoint.y == listePointPolygone.liste[i]->y){
-				existant = true;
-			}
-		}
-		if(!existant){
-			listePointPolygone.liste = (point **)realloc(listePointPolygone.liste, ++(listePointPolygone.nb_point)*sizeof(point));
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->x = nouveauPoint.x;
-			listePointPolygone.liste[listePointPolygone.nb_point-1]->y = nouveauPoint.y;
-			gen_point(lePolygone, nouveauPoint, distanceVoisin);
+			lePolygone->listePointPoly = (point **)realloc(lePolygone->listePointPoly, ++(lePolygone->nbPoint)*sizeof(point*));
+			lePolygone->listePointPoly[lePolygone->nbPoint - 1] = nouveauPoint;
+			gen_point(lePolygone, lePolygone->listePointPoly[lePolygone->nbPoint-1], distanceVoisin);
+			nouveauPoint = init_point();
 		}
 	}
 
+	// Point en bas
+	nouveauPoint->x = pointDepart->x;
+	nouveauPoint->y = pointDepart->y - distanceVoisin;
+	if(pointIsInPoly(nouveauPoint, lePolygone) == true){
+		bool existant = false;
+		for(int i = 0; i < lePolygone->nbPoint && existant == false; i++){
+			if(nouveauPoint->x == lePolygone->listePointPoly[i]->x && nouveauPoint->y == lePolygone->listePointPoly[i]->y){
+				existant = true;
+			}
+		}
+		if(!existant){
+			lePolygone->listePointPoly = (point **)realloc(lePolygone->listePointPoly, ++(lePolygone->nbPoint)*sizeof(point*));
+			lePolygone->listePointPoly[lePolygone->nbPoint - 1] = nouveauPoint;
+			gen_point(lePolygone, lePolygone->listePointPoly[lePolygone->nbPoint-1], distanceVoisin);
+		}
+	}
 }
--- gen_point.h
+++ gen_point.h
@@ -3,7 +3,51 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+typedef struct{
+	//Un sommet du polygone
+	point * sommetPoly;
+	//Liste de pointeur de point se trouvant à l'interieur du polygone
+	point ** listePointPoly;
+	//Nombre de point dans le poly hors sommet (nécessaire pour parcourir la liste)
+	int nbPoint;
+}polygone;
 
-bool pointIsInPoly(point, polygone);
-liste_point gen_point_polygone(polygone, point, float);
-void gen_point(polygone, point, float);
+/*
+* creerPoly();
+* Créér la structure polygone à partir d'un sommet;
+* Argument:
+* Un sommet du polygone
+* Return:
+* Une structure polygone
+*/
+polygone* creerPoly(point *);
+
+/*
+* pointIsInPoly();
+* Vérifie si un point via ces coordonnées se trouve dans le polygone
+* Argument:
+* Le point à vérifier et le polygone
+* Return:
+* Un booleen qui indique vrai si le point se trouve dans le polygone, faux sinon
+*/
+bool pointIsInPoly(point*, polygone*);
+
+/*
+* gen_point_polygone();
+* Vérifie si le point de départ se trouve bien dans le polygone, puis démarre la création de point
+* Argument:
+* Le polygone, le point de départ et la distance entre les voisins
+* Return:
+* Un booleen qui indique si la création a été effectuée.
+*/
+bool gen_point_polygone(polygone*, point*, float);
+
+/*
+* gen_point();
+* Génère les points de facon récursive;
+* Argument:
+* Le polygone, le point de départ et la distance entre les voisins
+* Return: 
+* Rien, la liste de point du polygone est modifié, il n'est donc pas nécessaire de renvoyer celui-ci
+*/
+void gen_point(polygone*, point*, float);
